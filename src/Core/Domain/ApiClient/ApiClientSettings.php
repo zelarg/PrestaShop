68e234919eb86711c301019ae3a30d17fc441915
Hlavtox <daniel.hlavacek@hotmail.cz>
2024-10-29 12:57:31 +0100

    Initial commit

--- src/Core/Domain/ApiClient/ApiClientSettings.php
+++ src/Core/Domain/ApiClient/ApiClientSettings.php
@@ -28,6 +28,8 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Core\Domain\ApiClient;
 
+use PrestaShopBundle\Form\Admin\Type\FormattedTextareaType;
+
 /**
  * Defines settings for API Client.
  */
@@ -35,5 +37,5 @@ class ApiClientSettings
 {
     public const MAX_CLIENT_ID_LENGTH = 255;
     public const MAX_CLIENT_NAME_LENGTH = 255;
-    public const MAX_DESCRIPTION_LENGTH = 21844;
+    public const MAX_DESCRIPTION_LENGTH = FormattedTextareaType::LIMIT_TEXT_UTF8;
 }
